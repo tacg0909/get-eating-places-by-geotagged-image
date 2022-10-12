746adb257cd902e30cc19a2b911260fb2caf350a
yu-ko-ba <yu-ko-ba@outlook.com>
2022-10-12 14:25:33 +0900

    返り値の変数をあらかじめ定義しておく方式に統一した

--- main.go
+++ main.go
@@ -58,20 +58,19 @@ type PlacesType struct {
     Results []PlaceType `json:"results"`
 }
 
-func fetchPlaces(lat float64, long float64) (PlacesType, error) {
+func fetchPlaces(lat float64, long float64) (places PlacesType, err error) {
     url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=%g,%g&language=ja&radius=10&key=%s", lat, long, os.Getenv("API_KEY"))
     resp, err := http.Get(url)
     if err != nil {
-        return PlacesType{}, err
+        return
     }
     defer resp.Body.Close()
 
     body, err := io.ReadAll(resp.Body)
     if err != nil {
-        return PlacesType{}, err
+        return
     }
 
-    var places PlacesType
     err = json.Unmarshal(body, &places)
 
     return places, err
